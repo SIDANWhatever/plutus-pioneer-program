46303633ef7007bd73c241aaa2ef31667e749304
SIDANWhatever <wongkahinhinson@gmail.com>
2023-03-18 05:27:02 +0000

    completing first 3 weeks hw

--- code/Week03/homework/Homework1.hs
+++ code/Week03/homework/Homework1.hs
@@ -8,11 +8,15 @@
 module Homework1 where
 
 import           Plutus.V2.Ledger.Api (BuiltinData, POSIXTime, PubKeyHash,
-                                       ScriptContext, Validator,
+                                       ScriptContext, TxInfo, Validator, scriptContextTxInfo, txInfoValidRange,
                                        mkValidatorScript)
 import           PlutusTx             (compile, unstableMakeIsData)
-import           PlutusTx.Prelude     (Bool (..))
+import           PlutusTx.Prelude     (Bool (..), ($), (&&), (||), (+))
 import           Utilities            (wrap)
+
+
+import           Plutus.V2.Ledger.Contexts (txSignedBy)
+import           Plutus.V1.Ledger.Interval (contains, to, from)
 
 ---------------------------------------------------------------------------------------------------
 ----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
@@ -29,7 +33,19 @@ unstableMakeIsData ''VestingDatum
 -- This should validate if either beneficiary1 has signed the transaction and the current slot is before or at the deadline
 -- or if beneficiary2 has signed the transaction and the deadline has passed.
 mkVestingValidator :: VestingDatum -> () -> ScriptContext -> Bool
-mkVestingValidator _dat () _ctx = False -- FIX ME!
+mkVestingValidator _dat () _ctx = (txSignerCheck (beneficiary1 _dat) && beforeDeadline) || (txSignerCheck (beneficiary2 _dat) && afterDeadline)
+    where
+        info :: TxInfo
+        info = scriptContextTxInfo _ctx
+
+        txSignerCheck :: PubKeyHash -> Bool
+        txSignerCheck = txSignedBy info
+
+        beforeDeadline :: Bool
+        beforeDeadline = contains (to $ deadline _dat) $ txInfoValidRange info
+
+        afterDeadline :: Bool
+        afterDeadline = contains (from (deadline _dat + 1)) $ txInfoValidRange info
 
 {-# INLINABLE  mkWrappedVestingValidator #-}
 mkWrappedVestingValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
--- code/Week03/homework/Homework2.hs
+++ code/Week03/homework/Homework2.hs
@@ -8,11 +8,14 @@
 module Homework2 where
 
 import           Plutus.V2.Ledger.Api (BuiltinData, POSIXTime, PubKeyHash,
-                                       ScriptContext, Validator,
+                                       ScriptContext, Validator, TxInfo, scriptContextTxInfo, txInfoValidRange,
                                        mkValidatorScript)
 import           PlutusTx             (applyCode, compile, liftCode)
-import           PlutusTx.Prelude     (Bool (False), (.))
+import           PlutusTx.Prelude     (Bool (..), (.), ($), (&&))
 import           Utilities            (wrap)
+
+import           Plutus.V2.Ledger.Contexts (txSignedBy)
+import           Plutus.V1.Ledger.Interval (contains, from)
 
 ---------------------------------------------------------------------------------------------------
 ----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
@@ -20,7 +23,16 @@ import           Utilities            (wrap)
 {-# INLINABLE mkParameterizedVestingValidator #-}
 -- This should validate if the transaction has a signature from the parameterized beneficiary and the deadline has passed.
 mkParameterizedVestingValidator :: PubKeyHash -> POSIXTime -> () -> ScriptContext -> Bool
-mkParameterizedVestingValidator _beneficiary _deadline () _ctx = False -- FIX ME!
+mkParameterizedVestingValidator _beneficiary _deadline () _ctx = txSignerCheck _beneficiary && afterDeadline
+    where
+        info :: TxInfo
+        info = scriptContextTxInfo _ctx
+
+        txSignerCheck :: PubKeyHash -> Bool
+        txSignerCheck = txSignedBy info
+
+        afterDeadline :: Bool
+        afterDeadline = contains (from _deadline) $ txInfoValidRange info
 
 {-# INLINABLE  mkWrappedParameterizedVestingValidator #-}
 mkWrappedParameterizedVestingValidator :: PubKeyHash -> BuiltinData -> BuiltinData -> BuiltinData -> ()
